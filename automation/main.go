b96635913f2a6bdf1ec3f37cd2c1837ff68677bf
andrey tsybin <cibon4ik@gmail.com>
2024-06-16 14:29:42 +0300

    add golang script to generate xray config from template filled with ansible vault variables

---
+++ automation/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	var (
+		secretsPath, xrayTemplatePath, xrayConfigPath string
+	)
+
+	flag.StringVar(&secretsPath, "secrets", "./secrets.yml", "path to ansible-vault secrets file")
+	flag.StringVar(&xrayTemplatePath, "xray-config-template", "./xray/reality_config_simple.template.json", "path to xray config template")
+	flag.StringVar(&xrayConfigPath, "xray-config-save", "./xray/reality_config_simple.done.json", "where you want to save finall config")
+
+	cmd := exec.Command("ansible-vault", "view", secretsPath, "--ask-vault-password")
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		log.Fatalf("Failed to run ansible-vault: %v", err)
+	}
+
+	var vaultConfig map[string]interface{}
+
+	err = yaml.Unmarshal(output, &vaultConfig)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal YAML: %v", err)
+	}
+
+	file, err := os.Open(xrayTemplatePath)
+
+	if err != nil {
+		log.Fatalf("Failed to open JSON file: %v", err)
+	}
+
+	defer file.Close()
+
+	var xrayConfig map[string]interface{}
+
+	decoder := json.NewDecoder(file)
+
+	if err := decoder.Decode(&xrayConfig); err != nil {
+		log.Fatalf("Failed to parse JSON: %v", err)
+	}
+	// template config
+	inbounds := xrayConfig["inbounds"].([]interface{})
+	settings := inbounds[0].(map[string]interface{})["settings"].(map[string]interface{})
+	streamSettings := inbounds[0].(map[string]interface{})["streamSettings"].(map[string]interface{})
+	realitySettings := streamSettings["realitySettings"].(map[string]interface{})
+	// vault config
+	reality := vaultConfig["xray"].(map[string]interface{})["reality"].(map[string]interface{})
+	clients := vaultConfig["clients"].([]interface{})
+
+	realitySettings["privateKey"] = reality["privateKey"].(string)
+	realitySettings["shortIds"] = append(realitySettings["shortIds"].([]interface{}), reality["shortId"].(string))
+	settings["clients"] = clients
+
+	jsonData, err := json.MarshalIndent(xrayConfig, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to marshal JSON: %v", err)
+	}
+
+	resultFile, err := os.Create(xrayConfigPath)
+
+	if err != nil {
+		log.Fatalf("Failed to create file: %v", err)
+	}
+	defer resultFile.Close()
+
+	_, err = resultFile.Write(jsonData)
+	if err != nil {
+		log.Fatalf("Failed to write JSON to file: %v", err)
+	}
+
+	fmt.Printf("Config has been written to %s", xrayConfigPath)
+}
